cc8cbb7c357bbd3e53f079ba603d12bbbe6d3ba7
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-10-01 18:18:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+const audio = document.getElementById('audio');
+let currentSound = null;
+
+function playSound(soundFileName) {
+    if (currentSound) {
+        stopSound();
+    }
+
+    audio.src = `sounds/${soundFileName}`;
+    audio.play();
+    currentSound = soundFileName;
+}
+
+function stopSound() {
+    audio.pause();
+    audio.currentTime = 0;
+    currentSound = null;
+}
